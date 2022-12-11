3c92195db99e11d1a550d9c64a75c6fb3a2e3504
BartekTrr <barte24@wp.pl>
2022-12-11 12:33:56 +0100

    Set up highlights

--- routes/highlights.js
+++ routes/highlights.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const fs = require('fs');
+const path = require('path')
 /* GET highlights listing. */
 router.get('/', function(req, res, next) {
-  res.render('highlights');
+    let data = fs.readFileSync(path.resolve(__dirname, "../data/highlights.json"));
+    res.render('highlights', { highlights: JSON.parse(data)});  
 });
 
 module.exports = router;
