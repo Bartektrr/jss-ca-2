0a4d8d7ce5fedf5702aec89b32050b72ee817448
BartekTrr <barte24@wp.pl>
2022-12-11 13:00:33 +0100

    Set up standings

--- routes/standings.js
+++ routes/standings.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path')
 
 /* GET standings listing. */
 router.get('/', function(req, res, next) {
-  res.render('standings');
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/standings.json"));
+  const sortedData = JSON.parse(data).sort((x,y) => {return x.position - y.position});
+  res.render('standings', { standings: sortedData});  
 });
 
 module.exports = router;
